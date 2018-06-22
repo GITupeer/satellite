39aee46e2416c98831cb1b9300be66534003d820
Patryk B <upeertv@gmail.com>
2018-06-22 17:20:29 +0200

    changes

--- index.php
+++ index.php
@@ -1,108 +1,62 @@
 <!DOCTYPE html>
 <html>
   <head>
-
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script> 
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
     <title>Satellite Radar</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-	
-	
   </head>
   <body> 
   
- <div style="
-    background: linear-gradient(141deg, #02183a 0%, #042559 51%, #07347a 75%);
-    color: white;
-    text-align: left;
-    padding: 5px;
-    padding-left: 10px;
-    position: fixed;
-    z-index: 1000;
-    width: 100%;
-    font-family: 'Roboto', sans-serif;
-">
+
 <div id="app">
-			<table border="0">
-				<tbody>
-					<tr>
-						<td>
-							<img src="https://image.ibb.co/jq7P4o/Maps_Satellites_icon.png" style="width: 41px; height: 41px;">
-						</td>
-						<td>
-							<div style="">Satellite Radar</div>
-							<div style="font-size:  10px;">Check position satellite in real time</div>
-						</td>
-						<td style="padding-left: 25px;">
-							<input type="text" name="search" placeholder="Search setallite..." style="border-radius: 4px;padding: 7px;font-size: 10px;width: 200px;">
-						</td>
-					</tr>
-				</tbody>
-			</table>
-				
-				
-			<div style="position: fixed; z-index: 1; right: 0; margin-top: -45px; color: #eaebed; margin-right: 17px; font-size: 21px;">
-				<div style="text-align: right;">
-					<span style="font-size: 9px;">UTC
-					</span>
-					<?php
-					echo date("H:i", strtotime ("+307 minute"));
-					?>
-				</div>
-				<div style="font-size: 12px; text-align: right;">
-					Satellite: <span style="font-weight: bold;">{{satelliteConuter}}</span>
-				</div>
-			</div>	
-				
+
+	<div class="topMenu" id="topMenu">
+		<table border="0">
+			<tbody>
+				<tr>
+					<td>
+						<img src="https://image.ibb.co/jq7P4o/Maps_Satellites_icon.png" style="width: 41px; height: 41px;">
+					</td>
+					<td>
+						<div style="">Satellite Radar</div>
+						<div style="font-size:  10px;">Check position satellite in real time</div>
+					</td>
+					<td style="padding-left: 25px;">
+						<input type="text" name="search" placeholder="Search setallite..." style="border-radius: 4px;padding: 7px;font-size: 10px;width: 200px;">
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<div style="position: fixed; z-index: 1; right: 0; margin-top: -45px; color: #eaebed; margin-right: 17px; font-size: 21px;">
+			<div style="text-align: right;">
+				<span style="font-size: 9px;">UTC
+				</span>
+				<?php
+				echo date("H:i", strtotime ("+307 minute"));
+				?>
+			</div>
+			<div style="font-size: 12px; text-align: right;">
+				Satellite: <span style="font-weight: bold;">{{satelliteConuter}}</span>
+			</div>
+		</div>	
+	</div>		
 
 				
 				   
 				 
-
-
-			</div>
-			<div style="
-				background-color: #F5F5F5;
-				border-right: 1px solid;
-				border-color: #9E9E9E;
-				height: 100%;
-				width: 250px;
-				position: fixed;
-				z-index: 2;
-				/* right: 0; */
-				color: white;
-				padding-top:  30px;
-				font-size: 11px;
-				/* background: linear-gradient(141deg, #02183a 0%, #042559 51%, #07347a 75%); */
-				margin-top: 48px;
-				box-shadow: 0 14px 8px 0 rgba(0,0,0,0.2), 0 16px 50px 0 rgba(0,0,0,0.19);
-			">
-				<div style="    width: 241px;    margin-top: -21px;    background-image: url(https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Ffe7fba9c-0298-11e8-a2b0-4e5c7848ab02.jpg?crop=5905%2C3321%2C148%2C576&amp;resize=685);height: 132px;    background-size: contain;    padding-top:  10px;    padding-left:  10px;    font-family: 'Roboto', sans-serif;font-size: 9px;">
-					@thetimes.co.uk 
-				</div>
-				<div style="background-color: #222121;margin: -2px;padding: 15px;font-family: 'Roboto', sans-serif;color: #FFD740;font-size: 15px;">
-					Space Station
-					<div style="font-size: 10px; margin-top: 2px; color: white;"> ISS SATELLITES </div>
-				</div>
-
-			<div style="margin-top: 8px; width: 100%; text-align: center;">
-
+	<div class="leftMenu" id="leftMenu">
+		<div style="    width: 241px;    margin-top: -21px;    background-image: url(https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Ffe7fba9c-0298-11e8-a2b0-4e5c7848ab02.jpg?crop=5905%2C3321%2C148%2C576&amp;resize=685);height: 132px;    background-size: contain;    padding-top:  10px;    padding-left:  10px;    font-family: 'Roboto', sans-serif;font-size: 9px;">
+			@thetimes.co.uk 
+		</div>
+		<div style="background-color: #222121;margin: -1px;padding: 15px; padding-left: 20px;font-family: 'Roboto', sans-serif;color: #FFD740;font-size: 15px;">
+			Space Station
+			<div style="font-size: 10px; margin-top: 2px; color: white;"> ISS SATELLITES </div>
+		</div>
+		<div style="margin-top: 8px; width: 100%; text-align: center;">
 			<table border="0" style="width: 97%; margin: 4px; color: #333;">
 				<tr>
 					<td  colspan="2" style="width: 49%; background-color: #E0E0E0; font-family: 'Roboto', sans-serif; font-size: 12px; padding-top: 2px; padding-bottom: 2px; font-weight: bold; text-align: center;">
@@ -126,8 +80,6 @@
 					</td>
 				</tr>
 			</table>
-
-
 
 			<table border="0" style="width: 97%; margin: 4px; color: #333; margin-top: 10px;">
 				<tr>
@@ -160,35 +112,36 @@
 					</td>
 				</tr>
 			</table>
+		</div>
+	</div>
+</div>
+<div id="map" style="width: 100%; height: 100%;"></div>			  
+				
+				
+				
+				
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true&key=AIzaSyDrwpP81uNX_HBYe7ncQqRgRsTAOQ5w4LU"></script>
+	<script>
+		var app = new Vue({
+			el: '#app',
+			data: {
+			satelliteConuter: 0,
+			marker: [],
+			map: [],
+			actualMarker: [],
+			map: ''
+			}, 
+			methods: {
+			
+				
+			},
+			computed: {
+				
 
-			 </div>
-
-			</div>
-			  
-				<div id="map"></div>
-				
-				
-				
-				<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-				<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDrwpP81uNX_HBYe7ncQqRgRsTAOQ5w4LU&callback=initMap"></script>
-				<script>
-				
-				
-			var app = new Vue({
-			  el: '#app',
-			  data: {
-				satelliteConuter: 0
-			  }, 
-			  methods: function(){
-				  
-				  
-			  },
-			  mounted: function(){
-					var marker;
-					var map;
-					var actualMarker;
-						function initMap() {
-							map = new google.maps.Map(document.getElementById('map'), {
+			},
+			mounted: function(){
+				var scope = this;
+				map = new google.maps.Map(document.getElementById('map'), {
 							zoom: 2,
 							center: {lat: -33.9, lng: 151.2},
 							styles: [
@@ -298,21 +251,22 @@
 						});
 
 							
-						
-						
-						setMarkers(map);
-					}
-											 
-				
-				function setMarkers(map) {
+
 					$.get('http://lim.bz/upeer/satellite/api.php').done(function(data){
 					var json = JSON.parse(data);
+					
+					scope.satelliteConuter = json.counter;
+
+
+			console.log('dsfds',scope.satelliteConuter);
+
+
+
+
 					var dataGeo = json.data;
 					console.log();
 					var array = JSON.parse("[" + dataGeo + "]");
 					var beaches = array;
-
-
 					var image = {
 					url: 'https://image.ibb.co/i9c5uo/sat24x24.png',
 					size: new google.maps.Size(24, 24),
@@ -327,8 +281,7 @@
 						var beach = beaches[i];
 						marker = new google.maps.Marker({
 							position: {lat: beach[1], lng: beach[2]},
-							animation: google.maps.Animation.DROP,
-							map: map,
+							map : map,
 							icon: image,
 							shape: shape,
 							title: beach[0],
@@ -336,40 +289,48 @@
 						});
 						actualMarker = beach[0];
 						
-						/*
-						google.maps.event.addListener(marker, "click", function (event) {
-							var flightPlanCoordinates = [
-							  {lat: 37.772, lng: -122.214},
-							  {lat: 21.291, lng: -157.821},
-							  {lat: -18.142, lng: 178.431},
-							  {lat: -27.467, lng: 153.027}
-							];
-							var flightPath = new google.maps.Polyline({
-							  path: flightPlanCoordinates,
-							  geodesic: true,
-							  strokeColor: '#FF0000',
-							  strokeOpacity: 1.0,
-							  strokeWeight: 2
-							});
-							
-							
-							flightPath.setMap(map); 
-							
-							console.log('DSFDSFDS', this.title);
-							
-							
-							
-						});*/
 						
-					}	
-				
-					});	
+							google.maps.event.addListener(marker, "click", function (event) {
+								var styleVal = document.getElementById("leftMenu").style.transform
+								console.log(styleVal);
+								if (styleVal == 'translateX(-105%)' || styleVal == ''){
+									document.getElementById("leftMenu").style.transform = "translateX(0)";	
+								} else {
+									document.getElementById("leftMenu").style.transform = "translateX(-105%)";
+								}
 
-				}			
+
+								
+								
+								/*var flightPlanCoordinates = [
+								{lat: 37.772, lng: -122.214},
+								{lat: 21.291, lng: -157.821},
+								{lat: -18.142, lng: 178.431},
+								{lat: -27.467, lng: 153.027}
+								];
+								var flightPath = new google.maps.Polyline({
+								path: flightPlanCoordinates,
+								geodesic: true,
+								strokeColor: '#FF0000',
+								strokeOpacity: 1.0,
+								strokeWeight: 2
+								});
+								
+								
+								flightPath.setMap(map); 
+								
+								console.log('DSFDSFDS', this.title);
+								*/
+								
+								
+							});
+						
+						}	
+
+					});
 				
-				
-			  }
-			})
+			}
+		})
 							
 				
 				
