b00840036f124efcc1a97e277853172f0f38255c
Patryk B <upeertv@gmail.com>
2018-06-27 13:52:58 +0200

    changes

--- api/app/Http/Controllers/APIController.php
+++ api/app/Http/Controllers/APIController.php
@@ -351,9 +351,32 @@ class APIController extends BaseController
     }
 
 
+    public function get_client_ip_env() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+     
+        return $ipaddress;
+    }
 
     public function userposition() {
-        $url = 'http://ip-api.com/json/'.$_SERVER['HTTP_CLIENT_IP'];
+
+    
+        $ip = $this->get_client_ip_env();
+
+        $url = 'http://ip-api.com/json/'.$ip;
 
         $json =  file_get_contents($url);
         $array = json_decode($json, true);
