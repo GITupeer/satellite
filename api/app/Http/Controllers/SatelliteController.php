dff554f32f911626c32c3fc2da662e4a960cabc4
Patryk B <upeertv@gmail.com>
2018-07-02 17:13:45 +0200

    sat

--- api/app/Http/Controllers/SatelliteController.php
+++ api/app/Http/Controllers/SatelliteController.php
@@ -190,7 +190,7 @@ class SatelliteController extends BaseController
                 $arr[$row['satellite_id']]['tle'] = $tle;
                 $explode_TLE_1 = explode(' ', $tle[0]);
                 if (!empty($explode_TLE_1[1])){
-
+                    $explode_TLE_1['tle'] = $tle;
                     echo '<pre>';
                     print_r($explode_TLE_1);
                     echo '</pre>'; exit;
