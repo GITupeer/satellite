a61cc44c89c694593f31d56c6640b4818ea8f99c
Patryk Blacha <upeertv@gmail.com>
2018-07-04 20:53:29 +0200

    changes

--- api/app/Http/Controllers/SatelliteController.php
+++ api/app/Http/Controllers/SatelliteController.php
@@ -327,7 +327,7 @@ class SatelliteController extends BaseController
                     $epochZeit = explode('.', $data['Epoka_TLE']);
 
 
-
+echo $tle;
                     echo '<pre>';
                     print_r($explode_TLE_1);
                     echo '</pre>'; exit;
