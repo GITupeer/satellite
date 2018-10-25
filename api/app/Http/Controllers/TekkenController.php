4f7beb95d67780e34bf60d9dc9a326ea81df72e6
upeer <upeertv@gmail.com>
2018-10-25 20:23:37 +0200

    tekken

--- api/app/Http/Controllers/TekkenController.php
+++ api/app/Http/Controllers/TekkenController.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 use Response;
 
 class TekkenController extends BaseController
@@ -33,6 +34,24 @@ class TekkenController extends BaseController
         }
 
     }
+
+
+
+    public function stworzTurniej() {
+        // Request $request
+        $user = 'upeer';
+        $password = 'pass';
+        $user = DB::table('tekken_user')->where([['user','=',$login], ['password','=',$pass]])->get();
+        $user = json_decode($user, true);
+
+
+        if (empty($user[0])){
+            return array('status' => 'error', 'message' => array( 'logowanie' => 'Login lub haslo jest nieprawidlowe')); exit;
+        }
+
+
+    }
+
 
 
 
