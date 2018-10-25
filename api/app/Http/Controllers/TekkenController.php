c0843a37f25cf981ed42576cf294b389ca253bdc
upeer <upeertv@gmail.com>
2018-10-25 16:55:35 +0200

    tekken

--- api/app/Http/Controllers/TekkenController.php
+++ api/app/Http/Controllers/TekkenController.php
@@ -15,6 +15,10 @@ class TekkenController extends BaseController
 
         $user = DB::table('tekken_user')->where([['user','=',$login]])->get();
         $user = json_decode($user, true);
+        echo '<pre>';
+        print_r($user);
+        echo '</pre>';
+        
         if (empty($login) || empty($pass)){
             return array('status' => 'error', 'message' => 'Login lub Haslo nie moze byc puste!'); exit;
         } else if (!empty($user[1])){
