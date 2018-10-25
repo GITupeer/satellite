b94076720bead9c771b6b4d4b322037392d1747a
upeer <upeertv@gmail.com>
2018-10-25 20:52:37 +0200

    tekken

--- api/public/index.php
+++ api/public/index.php
@@ -1,7 +1,5 @@
 <?php
-    header('Access-Control-Allow-Origin: *');  
-    header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, Content-Type, Accept"); 
-    header("Cache-Control: no-cache,no-store");
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){   
     exit;
 }
