8b673245e8a9f70f387c4aa81b2ca702caf9e9ae
upeer <upeertv@gmail.com>
2018-11-17 15:13:28 +0100

    changes

--- api/app/Http/Controllers/UpeerFinanseController.php
+++ api/app/Http/Controllers/UpeerFinanseController.php
@@ -7,9 +7,34 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Response;
+use App\Libraries\Push;
 
 class UpeerFinanseController extends BaseController
 {
+
+    const APPLICATION = '0240C-F21F3';
+    const BEARER = 'e55GkubTetiSWcEiONN5TmhPGDxKmKDV1TloxKfirH9K8Yu6DfamBZmArFlGEepC37ec4ABbuOfAGGDi7JPK';
+
+
+
+    public function sendPush($token, $body, $titile) {
+
+        $results = Push::sendNotificationToMany($titile, $body, $token);
+
+    }
+
+    
+
+    public function registerDevice($token) {
+        $insertUser = DB::table('upeer-app-users')->insert(
+            [
+            'push_token' => $token,
+            ]
+        );
+
+    }
+
+
 
     public function MCCStat() {
 
