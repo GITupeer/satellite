036882c78072f1d503e62ce94bc49694ab73f43d
upeer <upeertv@gmail.com>
2019-01-02 22:10:07 +0100

    Google Assistant

--- api/webhook/googleAssistant.php
+++ api/webhook/googleAssistant.php
@@ -3,24 +3,38 @@
 function sendMessage($parameters) {
     echo json_encode($parameters);
 }
+
+
+
+function scenarios ($queryText) {
+    $webhookText = "I'm sorry I do not know what you mean.";
+
+    if ($queryText == 'Saldo konto firmowe') {
+        $webhookText = 'The company account balance is PLN 23';   
+    }
+
+    sendMessage(array(
+        "source" => $update["responseId"],
+        "fulfillmentText" => $webhookText,
+        "payload" => array(
+            "items"=>[
+                array(
+                    "simpleResponse"=>
+                array(
+                    "textToSpeech"=>"Bad request"
+                     )
+                )
+            ],
+            ),
+       
+    ));
+}
+
+
  
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 
-     sendMessage(array(
-            "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook... ".$update["queryResult"]["queryText"],
-            "payload" => array(
-                "items"=>[
-                    array(
-                        "simpleResponse"=>
-                    array(
-                        "textToSpeech"=>"Bad request"
-                         )
-                    )
-                ],
-                ),
-           
-        ));
+
 
 ?>
